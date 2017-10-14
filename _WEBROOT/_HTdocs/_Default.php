383a0bd38d984a3b6c3be48a9089d3219b5b0ccc
OneBigOwnage <niekvandenbos@hotmail.com>
2017-10-14 19:11:45 +0200

    Changed the way we use _Default.php
    We now have a _Default.php in every directory. This file redirects to the main _Default.php in _WebRoot\_PHP\

--- _WEBROOT/_HTdocs/_Default.php
+++ _WEBROOT/_HTdocs/_Default.php
@@ -1,18 +1,4 @@
 <?php
-  // This file has to be included by every
-  // other php file, to manage certain defaults and rules.
-  session_start();
-  define('SERVER_ROOT', realpath($_SERVER['DOCUMENT_ROOT'].'/../'));
-  set_include_path(SERVER_ROOT.'/_PHP/');
-  require_once 'CONSTANTS.php';
-  require_once 'databaseManager.php';
-    Database::initConnection();
-  require_once 'logManager.php';
-    set_error_handler('LogManager::phpHandler');
-  require_once 'sessionManager.php';
-    SessionManager::init();
-    require_once 'Utils.php';
-
-  //By Default we need some Bootstrap and jQuery imports.
-  Utils::inclCSS('bootstrap.min.css', 'fa.min.css', 'master.css');
-  Utils::inclJS('jquery.min.js', 'bootstrap.min.js');
+$path = realpath($_SERVER['DOCUMENT_ROOT'].'/../_PHP/_Default.php');
+require_once $path;
+?>

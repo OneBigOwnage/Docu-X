cf9ecae7d0524b5967ea6d1a15519f61ea829542
OneBigOwnage <niekvandenbos@hotmail.com>
2017-10-21 13:27:13 +0200

    Added page template

---
+++ _WEBROOT/_HTdocs/_TEMPLATE.php
@@ -0,0 +1,13 @@
+<?php
+  require_once '_Default.php';
+  $page_name = '';
+  $page_title = "";
+
+  Utils::inclJS('');
+  Utils::inclCSS('');
+  ?>
+<?php include_once 'page_header.php';?>
+
+
+
+<?php include_once 'page_footer.php';?>

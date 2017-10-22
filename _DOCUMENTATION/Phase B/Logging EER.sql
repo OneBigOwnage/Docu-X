eba5c1bf307e541ae7f52aa1505b9583c0f9ec8d
OneBigOwnage <niekvandenbos@hotmail.com>
2017-10-22 13:32:05 +0200

    Split up UMS and Error logging documentation, created error loggin documentation

--- _DOCUMENTATION/Phase B/Logging EER.sql
+++ _DOCUMENTATION/Phase B/Logging EER.sql
@@ -1,9 +1,20 @@
-DROP TABLE IF EXISTS fwk_event_log;
-CREATE TABLE fwk_event_log (
+-- Queries to remove & re-generate the User Management System
+-- #############################################################################
+
+-- Remove all Foreign Keys
+ALTER TABLE fwk_error_log
+  DROP FOREIGN KEY IF EXISTS fk_fwk_error_log__to__ums_account;
+
+ALTER TABLE fwk_console
+	DROP FOREIGN KEY IF EXISTS fk_fwk_console__to__ums_account;
+-- #############################################################################
+
+-- Table definition for fwk_error_log.
+DROP TABLE IF EXISTS fwk_error_log;
+CREATE TABLE fwk_error_log (
 	id BIGINT (10) ZEROFILL AUTO_INCREMENT, -- Primary Key
-	message TEXT NOT NULL,
-	type VARCHAR (255) NOT NULL,
-	origin VARCHAR (255) DEFAULT 'undefined',
+	error_object JSON NOT NULL,
+	object_class VARCHAR (255) NOT NULL,
 	account_id BIGINT (10) ZEROFILL,	-- Foreign Key to ums_account.id
 
   -- Standard issue Create/Update/Delete datetimes.
@@ -13,5 +24,29 @@ CREATE TABLE fwk_event_log (
 	PRIMARY KEY (id)
 );
 
-ALTER TABLE fwk_event_log ADD CONSTRAINT FOREIGN KEY fk_fwk_event_log__to__ums_account
+-- Table definition for fwk_console.
+DROP TABLE IF EXISTS fwk_console;
+CREATE TABLE fwk_console (
+	id BIGINT (10) ZEROFILL AUTO_INCREMENT, -- Primary Key
+	output_text TEXT,
+	line_number INT,
+	file_name VARCHAR(255),
+	is_shown TINYINT(1),
+	account_id BIGINT (10) ZEROFILL,	-- Foreign Key to ums_account.id
+
+  -- Standard issue Create/Update/Delete datetimes.
+	c_dt DATETIME DEFAULT NOW(),
+	u_dt DATETIME ON UPDATE NOW(),
+	d_dt DATETIME,
+	PRIMARY KEY (id)
+);
+-- #############################################################################
+
+
+-- Setting of the Forreign Keys.
+ALTER TABLE fwk_error_log ADD CONSTRAINT FOREIGN KEY fk_fwk_error_log__to__ums_account
+  (account_id) REFERENCES ums_account (id);
+
+
+ALTER TABLE fwk_console ADD CONSTRAINT FOREIGN KEY fk_fwk_console__to__ums_account
   (account_id) REFERENCES ums_account (id);
